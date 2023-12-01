8289903adf260bd9388eb6b39f873396f9c4fd55
anshu_mandal <anshukrmandal7007@gmail.com>
2023-12-01 20:13:55 +0530

    inserting data

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -1,6 +1,7 @@
 const mongoose= require("mongoose");
-
-mongoose.connect("mongodb://0.0.0.0:27017//anshukr")
+// const MONGODB_URI_LOCAL=mongodb://localhost:27017/dbname
+const mongo_url= "mongodb://127.0.0.1:27017/anshukrmandal"
+mongoose.connect(mongo_url)
 .then( ()=>{console.log("connection successful")})
 .catch((err) =>{console.log(err)});
 
@@ -19,4 +20,23 @@ const playlistSchema = new mongoose.Schema({
     }
 })
 
-const Playlist = new mongoose.model("Playlist", playlistSchema);//playlist will be converted to playlists in mongodb collections
+const Playlist = new mongoose.model("Playlist", playlistSchema);//playlist will be converted to playlists in mongodb collections...... also the name should be in pascal convention or we can say first letter capital because it's a class
+
+// //creating document and insert
+const insertdoc = async() =>{
+    try{
+        const reactPlaylist = new Playlist({
+            name : "React JS2",
+            ctype : "Front end",
+            videos : 82,
+            author : "thapa technicals",
+            active : true
+        })
+
+        const doc = await reactPlaylist.save();
+        console.log(doc)
+    }catch(err){
+        console.log(err)
+    }
+}
+insertdoc()

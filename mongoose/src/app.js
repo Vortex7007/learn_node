97e2e224e756f30f9c97a4c1fd80f528415b6a38
anshu_mandal <anshukrmandal7007@gmail.com>
2023-12-01 10:32:46 +0530

    model and schema

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -3,3 +3,20 @@ const mongoose= require("mongoose");
 mongoose.connect("mongodb://0.0.0.0:27017//anshukr")
 .then( ()=>{console.log("connection successful")})
 .catch((err) =>{console.log(err)});
+
+const playlistSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    ctype : String,
+    videos : Number,
+    author : String,
+    active : Boolean,
+    date: {
+        type: Date,
+        default : Date.now
+    }
+})
+
+const Playlist = new mongoose.model("Playlist", playlistSchema);//playlist will be converted to playlists in mongodb collections
